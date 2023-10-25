8842f370b4ed65f215e0e0fffbcb50f7d4608c6c
Sebastian <ibarrahormaza18@gmail.com>
2023-10-25 09:09:26 -0500

    sexta version - tabla en facultades

--- facultades.php
+++ facultades.php
@@ -1,3 +1,25 @@
 <?php
-include 'index.php'
+    include 'index.php'
 ?>
+
+<h1>Listado de facultades</h1>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">codigo</th>
+      <th scope="col">nombre</th>
+      <th scope="col">opciones</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>123</td>
+      <td>artes</td>
+      <td>
+        <a class="btn btn-success">Editar</a>
+        <a class="btn btn-danger">Eliminar</a>
+      </td>
+    </tr>
+    
+  </tbody>
+</table>
