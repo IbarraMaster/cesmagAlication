b150552cb495f52250d924cb182f3f23f0eb83aa
Sebastian <ibarrahormaza18@gmail.com>
2023-10-25 08:46:32 -0500

    Cuarta version - Barra de navegacion

--- index.php
+++ index.php
@@ -5,10 +5,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cesmag</title>
+    <link rel="stylesheet" href="css/mdb.min.css">
+    <script src="js/mdb.min.js"></script>
 </head>
 <body>
-    <h1>Unicesmag</h1>
-    <h2>Nombre: Sebastian Ibarra</h2>
-    <h3>Email: ibarrahormaza18@gmail.com</h3>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Cesmag</a>
+        <button
+        class="navbar-toggler"
+        type="button"
+        data-mdb-toggle="collapse"
+        data-mdb-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+        >
+        <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+            <a class="nav-link" href="#">Facultades</a>
+            <a class="nav-link" href="#">Programas</a>
+           
+        </div>
+        </div>
+    </div>
+    </nav>
 </body>
 </html>
