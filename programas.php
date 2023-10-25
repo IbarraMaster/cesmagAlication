d6ad5c7ed73209559330114cc5e32abe395b5c00
Sebastian <ibarrahormaza18@gmail.com>
2023-10-25 09:11:31 -0500

    septima version - tabla en programas

--- programas.php
+++ programas.php
@@ -1,3 +1,23 @@
 <?php
     include 'index.php'
 ?>
+<h1>Listado de programas</h1>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">codigo</th>
+      <th scope="col">nombre</th>
+      <th scope="col">opciones</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>135</td>
+      <td>Ingenieria de sistemas</td>
+      <td>
+        <a class="btn btn-success">Editar</a>
+        <a class="btn btn-danger">Eliminar</a>
+      </td>
+    </tr>
+    
+  </tbody>
